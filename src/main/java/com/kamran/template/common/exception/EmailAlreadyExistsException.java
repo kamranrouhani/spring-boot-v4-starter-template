05d754c45fb9b2124cd4f3277f3f4a31382fde5e
comendrun <kamran.rouhani@outlook.com>
2026-01-03 02:23:00 +0100

    feat: add EmailAlreadyExistsException class for user management

--- src/main/java/com/kamran/template/common/exception/EmailAlreadyExistsException.java
+++ src/main/java/com/kamran/template/common/exception/EmailAlreadyExistsException.java
@@ -1,4 +1,12 @@
 package com.kamran.template.common.exception;
 
-public class EmailAlreadyExistsException {
+/**
+ * Thrown when attempting to create/update user with email that already exists.
+ * Results in 409 Conflict response.
+ */
+public class EmailAlreadyExistsException extends RuntimeException {
+
+    public EmailAlreadyExistsException(String email) {
+        super("Email already in use: "+ email);
+    }
 }
